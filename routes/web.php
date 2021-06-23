d2d585e8380666caefe275a3f582cbcd5f4f5bd3
Williams Martinez <williamisrael1601@gmail.com>
2021-06-23 17:23:04 -0400

    fb

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
 
-    Route::domain('admin.tt-import-pos-server.test')->group(function () {
+    Route::domain('admin.' . env('HOST'))->group(function () {
 
         Route::prefix('entities')->group(function () {
 
